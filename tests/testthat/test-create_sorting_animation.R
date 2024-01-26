9c9b9aa18de948b14443f4c7563b8f4bb035897a
John-Anthony-Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-01-26 06:44:35 +0000

    test to verify that gganimate outputs correctly

--- tests/testthat/test-create_sorting_animation.R
+++ tests/testthat/test-create_sorting_animation.R
@@ -4,12 +4,14 @@ test_that("create_sorting_animation handles different input types", {
   # Assuming bubble_sort_steps generates a matrix, convert it to different types
   N <- 5
   ssm_input <- bubble_sort_steps(N)
-  #ssm_input <- as.SortingStepsMatrix(matrix_input)
+
+  pg <- create_sorting_animation(ssm_input)
 
   # Test with matrix and data frame input
- #expect_silent(create_sorting_animation(matrix_input))
-  expect_silent(create_sorting_animation(ssm_input))
+  expect_equal(pg[1], "./gganim_plot0001.png")
 })
+
+
 
 test_that("find_differences_vectorized correctly identifies differences", {
   test_matrix <- matrix(c(1, 3, 2, 4, 1, 2, 3, 4), nrow = 2)
